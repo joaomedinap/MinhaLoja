2e962479aae90ae2c7e80dd167d7e70df8980d1c
Felipe Cardoso <felipebcardoso0711@outlook.com>
2025-10-21 21:44:09 -0300

    router

--- loja_cerveja/src/App.jsx
+++ loja_cerveja/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Lista_Produtos from './components/Lista_Produtos'
 import Carrinho from './components/Carrinho'
 
 function App() {
   return (
-    <>
-      <BrowserRouter basename="/MinhaLoja"></BrowserRouter>
+    <BrowserRouter basename="/MinhaLoja">
       <header className="bg-primary text-white text-center py-3 mb-4">
         <h1 className="m-0">Loja de Cervejas</h1>
       </header>
@@ -16,7 +16,7 @@ function App() {
       <footer className="bg-primary text-white text-center py-2 mt-auto">
         Direitos Autorais © 2025
       </footer>
-    </>
+    </BrowserRouter>
   )
 }
 
